20be745e975d0bb485b4c604d65b003832ff6039
Antony Braiano <78004742+Antony-Braiano@users.noreply.github.com>
2024-04-03 02:56:23 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,11 +12,6 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 # Remove hardware/lineage/compat to avoid conflicts
 rm -rf hardware/lineage/compat/Android.bp
 
-# Sepolicy fix for imsrcsd
-echo -e "${color}Switch back to legacy imsrcsd sepolicy${end}"
-rm -rf device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/imsservice.te
-cp device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/legacy-ims/hal_rcsservice.te device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/hal_rcsservice.te
-
 # Vendor & Kernel Sources
 git clone https://github.com/tanvirr007/vendor_xiaomi_spes -b 13.0 vendor/xiaomi/spes
 git clone https://github.com/tanvirr007/kernel_xiaomi_sm6225 kernel/xiaomi/sm6225
